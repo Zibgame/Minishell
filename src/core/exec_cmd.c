b3a30efd54877e31328dd9250baf92450e970cc1
Zibgame <fanizicy@gmail.com>
2025-12-05 09:22:14 +0100

    Add .supp

--- src/core/exec_cmd.c
+++ src/core/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 09:04:17 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/12/02 11:01:03 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/12/05 09:17:53 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ void	exec_cmd(t_shell shell, char *line)
 	char	*bin;
 
 	args = ft_split(line, ' ');
+	free(line);
 	if (!args)
 		return ;
 	bin = get_cmd(args[0], shell.env);
 	if (!bin)
 	{
 		printf("command not found: %s\n", args[0]);
+		free(line);
+		free(args);
 		exit(EXIT_FAILURE);
 	}
 	pid = fork();
@@ -32,8 +35,10 @@ void	exec_cmd(t_shell shell, char *line)
 	{
 		execve(bin, args, shell.env);
 		perror("execve");
+		free(args);
 		exit(EXIT_FAILURE);
 	}
 	waitpid(pid, NULL, 0);
+	free(args);
 	return ;
 }
