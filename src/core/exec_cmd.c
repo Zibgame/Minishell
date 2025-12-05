ca45cd528fa31c3d59f55e10c4aead79ef73ff8c
Zibgame <fanizicy@gmail.com>
2025-12-05 13:28:18 +0100

    save local changes

--- src/core/exec_cmd.c
+++ src/core/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: aeherve <aeherve@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 09:04:17 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/12/05 10:30:38 by aeherve          ###   ########.fr       */
+/*   Updated: 2025/12/05 13:23:18 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,12 @@ void	exec_cmd(t_shell shell, char *line)
 	char	*bin;
 
 	args = ft_split(line, ' ');
-	free(line);
 	if (!args)
 		return ;
 	bin = get_cmd(args[0], shell.env);
 	if (!bin)
 	{
 		printf("command not found: %s\n", args[0]);
-		free(line);
-		free(args);
 		exit(EXIT_FAILURE);
 	}
 	pid = fork();
@@ -35,10 +32,8 @@ void	exec_cmd(t_shell shell, char *line)
 	{
 		execve(bin, args, shell.env);
 		perror("execve");
-		free(args);
 		exit(EXIT_FAILURE);
 	}
 	waitpid(pid, NULL, 0);
-	free(args);
 	return ;
 }
