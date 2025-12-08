6264432010418f0791b791eed43be4ca5ae52b64
Zibgame <fanizicy@gmail.com>
2025-12-08 15:31:28 +0100

    Norm fic

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aeherve <aeherve@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 08:05:22 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/12/08 15:23:18 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/12/08 15:29:27 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@
 # include "structs.h"
 # include "librairies.h"
 
+/* == SIGNALS == */
 void		init_signal(void);
 void		sig_handler(int sig);
+
+/* == SHELL == */
 void		shell_loop(t_shell *shell);
 void		start_shell(t_shell *shell);
 void		exec_cmd(t_shell *shell, char *line);
-
 char		*read_line(void);
+t_shell		*create_shell_struct(char **envp);
+void		free_shell(t_shell *shell);
 
-/* == LKL == */
+/* == VARS LINKED LIST == */
 void		ft_lkladd_back(t_var_list **lst, t_var_list *new);
 void		ft_lkladd_front(t_var_list **lst, t_var_list *new);
 void		ft_lklclear(t_var_list **lst);
@@ -32,7 +36,10 @@ void		ft_lkldelone(t_var_list *lst);
 int			ft_lklsize(t_var_list *lst);
 t_var_list	*ft_lkllast(t_var_list *lst);
 t_var_list	*ft_lklnew(char *name, char *value);
+void		display_vars(t_var_list *vars);
+char		*get_value(t_shell *shell, char *name);
 
+/* == COMMAND LINKED LIST == */
 void		ft_cmdadd_back(t_cmd **lst, t_cmd *new);
 void		ft_cmdadd_front(t_cmd **lst, t_cmd *new);
 void		ft_cmdclear(t_cmd **lst);
@@ -40,22 +47,19 @@ void		ft_cmddelone(t_cmd *lst);
 int			ft_cmdsize(t_cmd *lst);
 t_cmd		*ft_cmdlast(t_cmd *lst);
 t_cmd		*ft_cmdnew(char *name, char **args);
+t_cmd		create_command(char *line);
 
-void		display_vars(t_var_list *vars);
-char		*get_value(t_shell *shell, char *name);
+/* == EXECUTION == */
+char		*get_cmd_path(t_shell *shell, char *cmd);
+int			finish(t_cmd *cmd, t_shell *shell);
 
-t_shell		*create_shell_struct(char **envp);
+/* == BUILTINS == */
+int			is_builtins(char *name);
+int			exec_builtins(t_cmd cmd, t_shell *shell);
+int			env(t_cmd *cmd, t_shell *shell);
+int			pwd(t_cmd *cmd, t_shell *shell);
 
-char	*get_cmd_path(t_shell *shell, char *cmd);
-int	is_builtins(char *name);
-int	exec_builtins(t_cmd cmd, t_shell *shell);
-int	env(t_cmd *cmd, t_shell *shell);
-int	pwd(t_cmd *cmd, t_shell *shell);
-t_cmd	create_command(char *line);
-int	print_header(char *path);
-
-int	finish(t_cmd *cmd, t_shell *shell);
-
-void	free_shell(t_shell *shell);
+/* == UTILS == */
+int			print_header(char *path);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aeherve <aeherve@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 08:05:18 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/12/08 14:55:54 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/12/08 15:31:20 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,5 @@ int	main(int argc, char **argv, char **envp)
 	if (!shell)
 		return (1);
 	start_shell(shell);
-	
 	return (0);
 }
