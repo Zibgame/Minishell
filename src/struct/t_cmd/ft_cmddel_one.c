ab9a205c89d207e2d6da97aba969aabc75b60cce
Dadounee <aedanherve@gmail.com>
2026-01-13 18:28:28 +0100

    redirs no leak

--- src/struct/t_cmd/ft_cmddel_one.c
+++ src/struct/t_cmd/ft_cmddel_one.c
@@ -6,7 +6,7 @@
 /*   By: dadoune <dadoune@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 12:03:23 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/12/09 20:24:05 by dadoune          ###   ########.fr       */
+/*   Updated: 2026/01/13 18:27:47 by dadoune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 void	ft_cmddelone(t_cmd *lst)
 {
+	t_redir	*tmp;
+	
 	if (!lst)
 		return ;
 	free(lst->name);
+	while (lst->redirs)
+	{
+		tmp = lst->redirs->next;
+		free(lst->redirs->target);
+		free(lst->redirs);
+		lst->redirs = tmp;
+	}
 	free(lst);
 }
