df892bf1b74c14e2770f29a5ee8ab1a5721b456d
Zibgame <fanizicy@gmail.com>
2026-01-19 16:54:20 +0100

    Update src/core/exec_cmd.c

--- src/core/exec_cmd.c
+++ src/core/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 09:04:17 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/01/19 13:52:22 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/01/19 16:53:53 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,56 +46,81 @@ int	handle_direct_path(t_shell *shell, char *cmd)
 	return (0);
 }
 
-void	exec_cmd(t_shell *shell, char *line)
+static char	**prepare_args(char *line)
 {
-	pid_t	pid;
-	int		status;
 	char	**args;
-	char	*path;
 
 	args = ft_split(line, ' ');
 	if (!args)
-		return ;
+		return (NULL);
 	args = clean_empty_args(args);
 	if (!args || !args[0])
 	{
 		free_array(args);
-		return ;
+		return (NULL);
 	}
-	path = NULL;
+	return (args);
+}
+
+static char	*resolve_path(t_shell *shell, char **args)
+{
+	char	*path;
+
 	if (ft_strchr(args[0], '/'))
 	{
 		if (handle_direct_path(shell, args[0]))
-		{
-			free_array(args);
-			return ;
-		}
-		path = ft_strdup(args[0]);
+			return (NULL);
+		return (ft_strdup(args[0]));
 	}
-	else
-		path = get_cmd_path(shell, args[0]);
+	path = get_cmd_path(shell, args[0]);
 	if (!path)
 	{
 		ft_printf_fd("minishell: %s: command not found\n", 2, args[0]);
-		return_type(shell, line);
-		free_array(args);
-		return ;
+		return_type(shell, args[0]);
 	}
-	pid = fork();
-	if (pid == 0)
-	{
-		signal_child();
-		if (apply_redirections(shell->cmd))
-			exit(1);
-		execve(path, args, shell->envp_tmp);
-		perror("execve");
-		exit(126);
-	}
+	return (path);
+}
+
+static void	exec_child(t_shell *shell, char *path, char **args)
+{
+	signal_child();
+	if (apply_redirections(shell->cmd))
+		exit(1);
+	execve(path, args, shell->envp_tmp);
+	perror("execve");
+	exit(126);
+}
+
+static void	handle_parent(t_shell *shell, pid_t pid)
+{
+	int	status;
+
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
 		shell->last_return = WEXITSTATUS(status);
 	else if (WIFSIGNALED(status))
 		shell->last_return = 128 + WTERMSIG(status);
+}
+
+void	exec_cmd(t_shell *shell, char *line)
+{
+	pid_t	pid;
+	char	**args;
+	char	*path;
+
+	args = prepare_args(line);
+	if (!args)
+		return ;
+	path = resolve_path(shell, args);
+	if (!path)
+	{
+		free_array(args);
+		return ;
+	}
+	pid = fork();
+	if (pid == 0)
+		exec_child(shell, path, args);
+	handle_parent(shell, pid);
 	free(path);
 	free_array(args);
 }
